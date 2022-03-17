2d3dc5e46a262bb001a84464e804f2384fd68709
ReihanBotZ <100804584+ReihanBotZ@users.noreply.github.com>
2022-03-17 20:48:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,14 +11,14 @@
 // thank you to ALL Bot creator
 // and thanks you to who support my Bot
 
-gc1 = ''
-gc2 = ''
-gc3 = ''
-global.linkGC = ['https://chat.whatsapp.com/HJ9pD2XJ8vq7whuAYOvT1Y', ''] // ganti jadi group lu
-global.owner = ['6289625556161','62896255561613'] // Put your number here //owner eval
-global.kontak = ['6289625556161','62896255561613'] //Ketika ada yang ngetik #owner
+gc1 = 'https://chat.whatsapp.com/HNosE6PyuIF19iNLDVb8Dm'
+gc2 = 'https://chat.whatsapp.com/HNosE6PyuIF19iNLDVb8Dm'
+gc3 = 'https://chat.whatsapp.com/HNosE6PyuIF19iNLDVb8Dm'
+global.linkGC = ['https://chat.whatsapp.com/HNosE6PyuIF19iNLDVb8Dm', ''] // ganti jadi group lu
+global.owner = ['6285368159958','6281379159390'] // Put your number here //owner eval
+global.kontak = ['6285368159958','6281379159390'] //Ketika ada yang ngetik #owner
 global.mods = ['0'] // Want some help?
-global.prems = ['6289625556161','601164215501','60166408295','601161197507','601156232697','601172962735','60182387590','60109211153','6281391552752'] // Premium user has unlimited limit
+global.prems = ['6285368159958','6281379159360','6283197699514','6281379159390','601156232697','601172962735','60182387590','60109211153','6281391552752'] // Premium user has unlimited limit
 global.APIs = { // API Prefix
   // name: 'https://website'
   rey: 'https://server-api-rey.herokuapp.com',
@@ -47,31 +47,31 @@ zekskey = 'apivinz'
 xteamkey = 'Dawnfrostkey'
 //xteam        MIMINETBOT
 namaig = 'GAK ADA :)'
-namagithub = 'https://github.com/MASTERMAN200'
+namagithub = 'https://github.com/ReihanBotZ'
 kasihcaption = `Nih kak`
-namakontak1 = 'Pilar Owner Bot'
-namakontak2 = 'Caca Admin Bot'
+namakontak1 = ' Owner Bot => YesarGanZ✓'
+namakontak2 = 'Admin Bot => Reihan JB✓'
 
 //kasihcaption = `Nih Kak`
 
 // Sticker WM
-global.packname = 'Zeus Botz' // ganti aja
-global.author = 'Zeus Botz' // ganti aja
+global.packname = 'ReihanBotZ' // ganti aja
+global.author = 'ReihanBotZ' // ganti aja
 
 //yyy
-bc = 'ZeusBotz' //ZeusBotz Broadcast
-footer = '\n© ZeusBotz'
-namabot = 'ZEUS BOTZ'
-namalu = 'PILAR'
-watermark = 'ZeusBotz' //ganti aja
+bc = 'ReihanBotz' //ReihanBotZ Broadcast
+footer = '\n© ReihanBotZ'
+namabot = 'ReihanBotZ'
+namalu = 'YesarGanZ✓'
+watermark = 'ReihanBotZ' //ganti aja
 
 // 
-wait = '_*Tunggu Sebentar...*_'
-global.botwm = 'ZeusBotz'
-global.wait = '_*Tunggu Sebentar...*_'
+wait = '_*[⏳] Tunggu Sebentar...*_'
+global.botwm = 'ReihanBotZ'
+global.wait = '_*[⏳] Tunggu Sebentar...*_'
 global.rpg = 'Fitur Rpg Dimatikan\nKetik *!enable* *rpg* untuk menggunakan fitur ini!\nKalo Mau main Disini aja\nhttps://chat.whatsapp.com/FnNAbem8o6r4pgLhSdO8Q9'
 global.nsfw = 'Fitur NSFW Dimatikan\nKetik *!enable* *nsfw* untuk menggunakan fitur ini!\n“Katakanlah kepada orang laki-laki yang beriman: Hendaklah mereka menahan pandanganya, dan memelihara kemaluannya; … Katakanlah kepada wanita yang beriman: Hendaklah mereka menahan pandangannya, dan kemaluannya, dan janganlah mereka Menampakkan perhiasannya, kecuali yang (biasa) nampak dari padany,” \n(TQS. Al-Nur [24]: 30-31).'
-global.eror = '_*Server Error*_'
+global.eror = '_*Server Error [❗]*_'
 
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=water-logo&doScale=true&scaleWidth=500&scaleHeight=500&fontsize=100&fillTextType=0&backgroundColor=%23101820&text='
 
